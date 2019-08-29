d41e754fef049062d968c618e3fd5aab0f8cba14
RakeshKrishna143 <41262164+RakeshKrishna143@users.noreply.github.com>
2019-08-29 21:03:11 +0530

    Update cryptoarthmetic.py

--- cryptoarthmetic.py
+++ cryptoarthmetic.py
@@ -1,33 +1,42 @@
 from itertools import permutations
-s1='KANSAS'
-s2='OHIO'
-s3='OREGON'
-s4='O'
-v=5
-a=[i for i in range(10)]
-a.remove(v)
-s5=(set(s1)|set(s2)|set(s3))-set(s4)
-s6=list(s5)
 
+s1=input()
+s2=input()
+s3=input()
+s4=input()
+v=int(input())
+
+if len(s1)>len(s2):
+    s1,s2=s2,s1
+d1={}
+d1[s4]=v
+if len(s1)<len(s2)<len(s3):
+    d1[s3[0]]=1 
+    d1[s3[1]]=0
+    d1[s2[0]]=9
+a=[i for i in range(10)]
+s5=(set(s1)|set(s2)|set(s3))
+s6=list(s5)
+for i,j in d1.items():
+    s6.remove(i)
+    a.remove(j)
 def value(d,s1):
     v=''
     for i in s1:
         v+=str(d[i])
     return int(v)
-
 def ans(d):
     v1=value(d,s1)
     v2=value(d,s2)
     v3=value(d,s3)
     if v1+v2==v3:
+        print(v1,v2,v3)
         return True
     return False
-
-
 b=list(permutations(a,len(s6)))
 for i in b:
     d={}
-    d[s4]=v
+    d.update(d1)
     for j,k in zip(s6,i):
         d[j]=str(k)
     if ans(d):
